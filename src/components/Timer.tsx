02340789ebfe930cee82b3f217979e21d420d1e6
tangrowth <to.chinatsu@icloud.com>
2023-11-16 00:04:33 +0900

    feat: タイマー機能とゲームの開始判定を追加

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,22 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import timerImage from '../assets/images/timer.png';
-import resetButton from '../assets/images/stop-button.png'
-import { clear } from 'console';
+import resetButton from '../assets/images/stop-button.png';
 
-function Timer() {
-  /** 制限時間の設定 */
+const Timer = () => {
+  const [time, setTime] = useState(0); //制限時間を設定
+  const [isStarted, setIsStarted] = useState(false); //ゲームの開始を管理
 
-  /** 制限時間のリセットの設定 */
+    /** isStartedの値が変更されると実行 */
+  useEffect(() => {
+    if (!isStarted) {
+      return;
+    }
+    /**
+     * 毎秒1ずつカウントを減らす 
+     * タイマーが0以下になるとゲームが開始されていない状態にする
+     */
+    const timerId = setInterval(() => {
+      setTime((prevTime) => prevTime - 1);
+      if (time < 1) {
+        setIsStarted(false);
+      }
+    }, 1000);
+    /** ゲームが開始されていない時はタイマーを止める */
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [isStarted, time]);
 
-  /** 表示するタイマー */
+  /** ゲームが開始されると時間をリセット */
+  function startGame() {
+    setTime(100);
+    setIsStarted(true);
+    }
+
+  /** ゲームが開始されていない状態にする */
+  function resetGame() {
+    setIsStarted(false);
+  }
+
   return (
     <div>
-      <div>
-        <img src={timerImage} alt="" />制限時間: ~s
-      </div>
-      <img src={resetButton} alt="リセット" />
+      {isStarted ? (
+        <div>
+          <img src={resetButton} alt="リセット" onClick={resetGame} />
+          <img src={timerImage} alt="" />残り時間: {time}s
+        </div>
+      ): (
+        <button onClick={startGame}>ゲームをはじめる</button>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Timer;
