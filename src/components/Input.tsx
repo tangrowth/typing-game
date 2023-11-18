a60360603e6409fb9c1f00abebd2c19d06bd1fe2
tangrowth <to.chinatsu@icloud.com>
2023-11-18 19:58:15 +0900

    feat: ワードリストの追加とデータの取得

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import wordData from '../data/word.json';
 
 
 function Input() {
-  /** お題の取得 */
-  const [currentWord, setCurrentWord] = useState("Apple"); //お題
-  const [translatedWord, setTranslatedWord] = useState("りんご"); //お題の日本語訳
+  const [wordList, setWordList] = useState(wordData.words); //ワードリストの取得
+  const [currentWord, setCurrentWord] = useState(''); //お題
+  const [translatedWord, setTranslatedWord] = useState(''); //お題の日本語訳
   const [inputWord, setInputWord] = useState('');
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState("不正解");
 
-  /** 入力欄との検証 */
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    const randomWord = wordList[randomIndex];
+    setCurrentWord(randomWord.currentWord);
+    setTranslatedWord(randomWord.translatedWord);
+  }, [result, wordList]);
+
   useEffect(() => {
     if (inputWord === currentWord) {
       setResult("正解！");
+      setInputWord('');
     } else {
       setResult("不正解");
     }
   }, [inputWord, currentWord]);
 
-  /** 画面の表示 */
   return (
     <div>
       <div>{ translatedWord }</div>
