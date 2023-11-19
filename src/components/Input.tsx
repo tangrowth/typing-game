836c9b8992ad69dd57a3577d95402934d1f9b207
tangrowth <to.chinatsu@icloud.com>
2023-11-20 00:16:01 +0900

    feat: TimerコンポーネントとInputコンポーネント間のデータのやり取りを設定

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import wordData from '../data/word.json';
 
-
-function Input() {
+function Input({ isStarted, setIsStarted }: {
+  isStarted: boolean, setIsStarted: React.Dispatch<React.SetStateAction<boolean>>
+}) {
   const [wordList, setWordList] = useState(wordData.words); //ワードリストの取得
   const [currentWord, setCurrentWord] = useState(''); //お題
   const [translatedWord, setTranslatedWord] = useState(''); //お題の日本語訳
   const [inputWord, setInputWord] = useState('');
-  const [result, setResult] = useState("不正解");
+  const [result, setResult] = useState(false);
 
+  function handleIsStarted() {
+    setIsStarted(true);
+  }
+
+  /** 正解するたびにあたらしい単語を取得 */
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * wordList.length);
     const randomWord = wordList[randomIndex];
@@ -16,28 +22,37 @@ function Input() {
     setTranslatedWord(randomWord.translatedWord);
   }, [result, wordList]);
 
+  //入力の正誤を判定
   useEffect(() => {
     if (inputWord === currentWord) {
-      setResult("正解！");
+      setResult(true);
       setInputWord('');
     } else {
-      setResult("不正解");
+      setResult(false);
     }
   }, [inputWord, currentWord]);
 
   return (
     <div>
-      <div>{ translatedWord }</div>
-      <div>{ currentWord }</div>
-      <input
-        type="text"
-        name="inputWord"
-        value={inputWord}
-        onChange={(e) => {
-          setInputWord(e.target.value)
-        }}
-      />
-      {result}
+      {isStarted ?(
+        <div>
+          <div>{translatedWord}</div>
+          <div>{currentWord}</div>
+          <input
+            type="text"
+            name="inputWord"
+            value={inputWord}
+            onChange={(e) => {
+              setInputWord(e.target.value)
+            }}
+            disabled={!isStarted}
+          />
+        </div>
+      ) : (
+        <div>
+          <button onClick={handleIsStarted}>ゲームをはじめる</button>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,36 +2,40 @@ import React, { useEffect, useState } from 'react';
 import timerImage from '../assets/images/timer.png';
 import resetButton from '../assets/images/stop-button.png';
 
-const Timer = () => {
-  const [time, setTime] = useState(0); //制限時間を設定
-  const [isStarted, setIsStarted] = useState(false); //ゲームの開始を管理
+const Timer = ({ isStarted, setIsStarted }: {
+  isStarted: boolean, setIsStarted: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
+  const [time, setTime] = useState(0); // 制限時間を設定
 
-    /** isStartedの値が変更されると実行 */
+  /** isStartedの値が変更されると実行 */
   useEffect(() => {
     if (!isStarted) {
       return;
     }
+
+    // タイマーが100にセットされる
+    setTime(100);
+
     /**
      * 毎秒1ずつカウントを減らす 
      * タイマーが0以下になるとゲームが開始されていない状態にする
      */
     const timerId = setInterval(() => {
-      setTime((prevTime) => prevTime - 1);
-      if (time < 1) {
-        setIsStarted(false);
-      }
+      setTime((prevTime) => {
+        if (prevTime <= 1) {
+          setIsStarted(false);
+          clearInterval(timerId);
+          return 0;
+        }
+        return prevTime - 1;
+      });
     }, 1000);
+
     /** ゲームが開始されていない時はタイマーを止める */
     return () => {
       clearInterval(timerId);
     };
-  }, [isStarted, time]);
-
-  /** ゲームが開始されると時間をリセット */
-  function startGame() {
-    setTime(100);
-    setIsStarted(true);
-    }
+  }, [isStarted, setIsStarted]);
 
   /** ゲームが開始されていない状態にする */
   function resetGame() {
@@ -39,16 +43,14 @@ const Timer = () => {
   }
 
   return (
-    <div>
-      {isStarted ? (
-        <div>
-          <img src={resetButton} alt="リセット" onClick={resetGame} />
-          <img src={timerImage} alt="" />残り時間: {time}s
-        </div>
-      ): (
-        <button onClick={startGame}>ゲームをはじめる</button>
-      )}
-    </div>
+    isStarted ? (
+      <div>
+        <img src={resetButton} alt="リセット" onClick={resetGame} />
+        <img src={timerImage} alt="" />残り時間: {time}
+      </div>
+    ) : (
+        <div></div>
+    )
   );
 };
 
